a2178f48ebfc44ee1056c1935dae6bca3f3e4619
Pavel Karpenko <ospkiev@gmail.com>
2019-04-05 13:09:27 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict'
+
+let form = document.querySelector('form');
+let inputTitle = document.querySelector('inputTitle');
+let inputTask = document.querySelector('inputTask');
+let button = document.querySelector('button');
+let list = document.querySelector('list');
