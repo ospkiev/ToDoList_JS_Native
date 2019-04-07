692056de417a56af0f0dd96a689fec4bb4ed44db
Pavel Karpenko <ospkiev@gmail.com>
2019-04-07 19:44:10 +0300

    with sort func

--- index.js
+++ index.js
@@ -1,78 +1,112 @@
 'use strict'
 
-let form = document.querySelector('.form');
-let inputTitle = document.querySelector('.inputTitle');
-let inputTask = document.querySelector('.inputTask');
-let button = document.querySelector('.button');
-let list = document.querySelector('.list');
-let deleteItem = document.createElement('span');
-deleteItem.textContent = 'delete';
-// let editItem = document.createElement('span');
-// editItem.textContent = 'edit';
+const form = document.querySelector('.form');
+const inputTitle = document.querySelector('.inputTitle');
+const inputTask = document.querySelector('.inputTask');
+const button = document.querySelector('.button');
+const btnSortByTime = document.querySelector('.btnSortByTime');
+const list = document.querySelector('.list');
+const now = new Date().getTime();
+const date = new Date(now).toString('');
 
+let arrayTask = [];
 
-
-function addTask(e) {
-    e.preventDefault();
-
+function card(timeParam, titleParam, taskParam, deleteParam, editParam) {
     let li = document.createElement('li');
+    let title = document.createElement('h2');
+    let task = document.createElement('h3');
     let time = document.createElement('p');
-    let title = document.createElement('h3');
-    let task = document.createElement('p');
-
+    let deleteItem = document.createElement('span');
+    deleteItem.setAttribute('data-id', timeParam)
+    let editItem = document.createElement('span');
 
     list.append(li);
     li.append(title);
     li.append(task);
     li.append(time);
+    li.append(editItem);
+    li.append(deleteItem);
+    time.textContent = timeParam;
+    title.textContent = titleParam;
+    task.textContent = taskParam;
+    deleteItem.textContent = deleteParam;
+    editItem.textContent = editParam;
+}
 
-    let now = new Date().getTime();
-    let date = new Date(now);
-    time.textContent = date.toString('');
+function addTask() {
 
-    title.textContent = inputTitle.value;
-    task.textContent = inputTask.value;
+    // let date = new Date().toString('');
+    let date = Date.now();
 
-    let taskItem = {
-        id: date,
-        task: inputTask.value,
+    const taskItem = {
         title: inputTitle.value,
+        task: inputTask.value,
+        time: date,
+        delete: 'delete',
+        edit: 'edit',
     }
 
+    // arrayTask.push(taskItem);
     let itemList = JSON.parse(localStorage.getItem('taskItem')) || [];
     itemList.push(taskItem);
     localStorage.setItem('taskItem', JSON.stringify(itemList));
-
     inputTitle.value = '';
     inputTask.value = '';
+    // renderArr();
 }
 
-// function deleteItemFromLS(e) {
+function sortByTime(e) {
+    e.preventDefault();
+    let sortedArr = [];
+
+    sortedArr = arrayTask.sort(function (a, b) {
+        if (b.time < a.time) {
+            return b.time - a.time
+        } else {
+            return a.time - b.time
+        }
+    })
+
+    arrayTask = sortedArr;
+    localStorage.setItem('taskItem', JSON.stringify(arrayTask));
+    console.log(arrayTask);
+    location.reload();
+    // getListTask();
+    // renderArr();
+}
+
+function deleteItem(e) {
+    let id = e.target.dataset.id;
+    let result = JSON.parse(localStorage.getItem('taskItem')) || [];
+    let deletedItem = result.filter(el => el.time !== +id);
+    localStorage.setItem('taskItem', JSON.stringify(deletedItem));
+    location.reload();
+    // getListTask();
+}
+
+// function editItem(e) {
 //     let id = e.target;
 //     console.log(id);
 // }
 
 function getListTask() {
     let result = JSON.parse(localStorage.getItem('taskItem')) || [];
-    console.log(result);
-    result.map(el => {
-        let li = document.createElement('li');
-        let time = document.createElement('p');
-        let title = document.createElement('h3');
-        let task = document.createElement('p');
+    arrayTask = result;
+    // console.log(result);
+    renderArr();
+}
 
-        list.append(li);
-        li.append(title);
-        li.append(task);
-        li.append(time);
-
-        title.textContent = el.title;
-        task.textContent = el.task;
-        time.textContent = el.id;
-    })
+function renderArr() {
+    arrayTask.map(el =>
+        card(el.time, el.title, el.task, el.delete, el.edit)
+    );
+    // console.log(arrayTask);
 }
 
 
 form.addEventListener('submit', addTask);
-// window.addEventListener('click', deleteItemFromLS);
+list.addEventListener('click', deleteItem);
+btnSortByTime.addEventListener('click', sortByTime);
+// window.addEventListener('click', editItem);
 window.addEventListener('DOMContentLoaded', getListTask);
+
